6bc8d5af31c10bb8bf741a7f0aa628432099dfdc
wangzhou666 <wdx6666666@gmail.com>
2015-07-10 11:29:20 -0500

    hw0

---
+++ hw0/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+	public static void main(String[] args) {
+		int year = 2004;
+		if (year%400 == 0) {
+			System.out.println(year+" is a leap year.");
+		} else if ((year%4 == 0)||(year%100 == 0)) {
+			System.out.println(year+" is a leao year..");
+		}
+	}
+}
