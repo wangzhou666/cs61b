c878e0bb45416e4f4001d1f7db4702ced2fc1e4f
wangzhou666 <wdx6666666@gmail.com>
2015-07-10 16:17:24 -0500

    hw0 done

---
+++ hw0/MaxElement.java
@@ -0,0 +1,15 @@
+public class MaxElement {
+   public static int max(int[] m) {
+   		int temp = m[0];
+   		for (int i : m) {
+   			if (temp < i){
+   				temp = i;	
+   			}
+   		}
+       	return temp;
+   }
+   public static void main(String[] args) {
+      int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};  
+      System.out.print(max(numbers));    
+   }
+}
