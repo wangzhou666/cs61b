09322be736685578aea94677908dbc72d5c540ff
wangzhou666 <wdx6666666@gmail.com>
2015-07-21 21:24:13 -0500

    proj1 part3 done

---
+++ proj1/ngordnet/TimeSeries.java
@@ -0,0 +1,68 @@
+package ngordnet;
+import java.util.Collection;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TimeSeries<T extends Number> extends TreeMap<Integer, T> {
+
+	public TimeSeries() {
+		super();
+	}
+
+	public Collection<Number> years() {
+		Set<Integer> setOfKeys = keySet();
+		Collection<Number> collectionOfKeys = new TreeSet<Number>();
+		for (Integer num : setOfKeys) {
+			collectionOfKeys.add(num);
+		}
+		return collectionOfKeys;
+	}
+
+	public Collection<Number> data() {
+		Set<Integer> setOfKeys = keySet();
+		Collection<Number> collectionOfData = new TreeSet<Number>();
+		for (Integer num : setOfKeys) {
+			collectionOfData.add(get(num));
+		}
+		return collectionOfData;
+	}
+
+	public T get(Integer key) {
+		return super.get(key);
+	}
+
+	public TimeSeries<Double> plus(TimeSeries x) {
+		TimeSeries<Double> newSeries = new TimeSeries<Double>();
+		Set<Integer> setOfThis = keySet();
+		Set<Integer> setOfX = x.keySet();
+		for (Integer num : setOfThis) {
+			if (setOfX.contains(num)) {
+				Double dVal = new Double(get(num).doubleValue() + x.get(num).doubleValue());
+				newSeries.put(num, dVal);
+			} else {
+				Double dVal = new Double(get(num).doubleValue());
+				newSeries.put(num, dVal);
+			}
+		}
+		for (Integer num : setOfX) {
+			if (setOfThis.contains(num)) {
+				continue;
+			} else {
+				Double dVal = new Double(x.get(num).doubleValue());
+				newSeries.put(num, dVal);
+			}
+		}
+		return newSeries;
+	}
+
+	public TimeSeries<Double> dividedBy(TimeSeries x) {
+		TimeSeries<Double> newSeries = new TimeSeries<Double>();
+		Set<Integer> setOfKeys = keySet();
+		for (Integer num : setOfKeys) {
+			Double dVal = new Double(get(num).doubleValue() / x.get(num).doubleValue());
+			newSeries.put(num, dVal);
+		}
+		return newSeries;
+	}
+}
